1e629279fde2ff9e9e7610b3c9be5fafe45f6486
isaac1212 <nsambai722@gmail.com>
2021-11-10 11:35:34 +0300

    database config

--- config/database.js
+++ config/database.js
@@ -17,10 +17,13 @@ module.exports = ({ env }) => {
             database: config.database,
             username: config.user,
             password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },
           },
-          options:{
-            ssl: false
-          }
+          options: {
+            ssl: env.bool('DATABASE_SSL', false),
+          },
         }
       }
     }
