77451eefc34dbe986535dbe2c2a3bdabef4043a1
isaac1212 <nsambai722@gmail.com>
2021-11-10 14:46:14 +0300

    admin panel edit

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoStrapi from '../../../../assets/images/logo_enjiri.png';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoStrapi} alt="enjiri-logo" />;
+
+export default Logo;
