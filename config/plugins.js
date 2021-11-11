84411763734320f7ed0e2797b054f79a0f45854c
isaac1212 <nsambai722@gmail.com>
2021-11-11 11:27:53 +0300

    aws keys

--- config/plugins.js
+++ config/plugins.js
@@ -7,10 +7,10 @@ module.exports = ({ env }) => {
         provider: 'aws-s3',
         providerOptions: {
           accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-          region: env('us-east-1'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
           params: {
-            Bucket: env('AWS_S3_BUCKET'),
+            Bucket: env('AWS_BUCKET'),
           },
         },
       },
