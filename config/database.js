9dac1ca1ae7f826b171b29f3fd4415784e2b439c
isaac1212 <nsambai722@gmail.com>
2021-12-07 01:15:48 +0300

    backend

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-const parse = require('pg-connection-string').parse;
+// const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
   
   if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL);
+    // const config = parse(process.env.DATABASE_URL);
    
     return {
       defaultConnection:'default',
@@ -11,19 +11,33 @@ module.exports = ({ env }) => {
         default:{
           connector: 'bookshelf',
           settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            },
+            client: 'mysql',
+            host: process.env.DATABASE_HOST,
+            port: process.env.DATABASE_PORT,
+            database: process.env.DATABASE_NAME,
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+          },
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
           },
           options: {
-            ssl: env.bool('DATABASE_SSL', false),
+             useNullAsDefault: true,
           },
+          // settings: {
+          //   client: 'postgres',
+          //   host: config.host,
+          //   port: config.port,
+          //   database: config.database,
+          //   username: config.user,
+          //   password: config.password,
+          //   ssl: {
+          //     rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          //   },
+          // },
+          // options: {
+          //   ssl: env.bool('DATABASE_SSL', false),
+          // },
         }
       }
     }
